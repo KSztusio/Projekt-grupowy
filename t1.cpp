6b464ff5447973403f822d671e1ac3c16b5ccdbb
Michał Pskit <apskit@wp.pl>
2025-01-26 22:43:00 +0100

    Update t1.cpp

--- t1.cpp
+++ t1.cpp
@@ -2,181 +2,155 @@
 #include <fstream>
 #include <string>
 #include <cmath>
-#include <thread>
 #include <vector>
-#include <mutex>
-
-#define LICZBA_WATKOW 6
 
 using namespace std;
 
-struct Result {
-    double modulus;
-};
-
-double first_lines[LICZBA_WATKOW];
-
-
-// Przetwarzanie fragmentu pliku
-void process_file_chunk(fstream& file, streampos start, streampos end, int thread_id, vector<Result>& results) {
-    file.seekg(start);
-    string line, x, y;
-    double xf, yf, w;
-    int licznik = 0;
-    bool first = true;
-
-    // Cofanie kursora do początku linii 
-    char ch;
-    while (file.tellg() > 1) {
-        file.seekg(-1, ios_base::cur);
-        ch = file.get();
-        if (ch == '\n') { break; }
-        file.seekg(-1, ios_base::cur);
-    }
-
-    // Przetwarzanie fragmentu
-    while (file.tellg() < end && getline(file, line)) {
-        x.clear();
-        y.clear();
-        x = y = "";  // Czyszczenie tymczasowych stringów
-        if (line.length() > 4 && line[1] == '(') {
-            // Przetwarzanie linii z punktami (x, y)
-            bool g = true;
-            for (int i = 2; i < line.length() - 1; i++) {
-                if (line[i] != ',') {
-                    if (g) x += line[i];
-                    else y += line[i];
-                }
-                else {
-                    g = false;
-                }
-            }
-            xf = stod(x);
-            yf = stod(y);
-            w = sqrt((xf * xf) + (yf * yf));
-            results.push_back({w});
-            if (first) {
-                first = false;
-                first_lines[thread_id] = w;
-            }
-
-            licznik++;
-        }
-    }
-    //cout << thread_id << "         " << xf << " " << yf <<"   "<<line<< endl;
-
-    // Sprawdzenie czy po koncu tego fragmentu jest poczatek kolejnego
-    if (thread_id != LICZBA_WATKOW) {
-        getline(file, line);
-        x.clear();
-        y.clear();
-        x = y = "";  // Czyszczenie tymczasowych stringów
-        if (line.length() > 4 && line[1] == '(') {
-            // Przetwarzanie linii z punktami (x, y)
-            bool g = true;
-            for (int i = 2; i < line.length() - 1; i++) {
-                if (line[i] != ',') {
-                    if (g) x += line[i];
-                    else y += line[i];
-                }
-                else {
-                    g = false;
-                }
-            }
-            xf = stod(x);
-            yf = stod(y);
-            w = sqrt((xf * xf) + (yf * yf));
-        }
-        if (w != first_lines[thread_id + 1]) {
-            results.push_back({w});
-        }
-
-        // Odczyt ostatniego wektora - sprawdzenie duplikacji z kolejnym
-        if (results.back().modulus == first_lines[thread_id + 1]) {
-            results.pop_back(); // usuniecie ostatniego elementu
-        }
-    }
-}
+double string_to_double(string number);
 
 int main(int argc, char* argv[]) {
-    int tstart = clock();
-
+    int k, columns_n, n1_n, n2_n, step;
     string file_name = "data.mat";
     if (argc > 1) {
         file_name = argv[1];
     }
 
-    for (int i = 0; i < LICZBA_WATKOW; i++) {
-        first_lines[i] = -1;
-    }
+    string b, name, type, x, y, columns, n1, n2, deadzone;
+    fstream plik;
+    fstream plikzap;
+    plik.open(file_name, ios::in);
+    plikzap.open("dane.txt", ios::out);
 
-    fstream plik(file_name, ios::in);
+    if (plik.is_open() && plikzap.is_open()) {
+        int j = 0;
+        while (!plik.eof()) {
+            getline(plik, b);
+            if (b[0] == '#') {
+                if (b.substr(2, 4) == "name") {
+                    name = "";
+                    for (int i = 8; i < b.length(); i++) {
+                        name += b[i];
 
-    // Znajdź punkt startowy danych (signal)
-    string line;
-    streampos signal_start = 0;
-    while (getline(plik, line)) {
-        if (line.find("signal") != string::npos) {
-            getline(plik, line);
-            getline(plik, line);
-            getline(plik, line); // TODO
+                    }
+                }
+            }
+            if (name == "azimuthRaster") {
+                if (b[0] == '#') {
+                    if (b.substr(2, 8) == "columns:") {
+                        columns = "";
+                        for (int l = 11; l < b.length(); l++) {
+                            columns += b[l];
+                            columns_n = stod(columns);
+                        }
+                    }
+                }
+                else if (b[0] == ' ') {
+                    k = 1;
+                    n1 = n2 = "";
+                    while (b[k] != ' ') {
+                        n1 += b[k];
+                        n1_n = string_to_double(n1);
+                        k++;
+                    }
+                    k++;
+                    while (b[k] != ' ') {
+                        n2 += b[k];
+                        n2_n = string_to_double(n2);
+                        k++;
+                    }
+                    step = n2_n - n1_n;
+                    name = "";
+                    plikzap << "azimuth\n" << n1 << ' ' << step << ' ' << columns_n - 1 << '\n';
 
-            signal_start = plik.tellg(); // Pozycja za znalezionym "signal"
-            break;
+                }
+            }
+            else if (name == "distanceRaster") {
+                if (b[0] == '#') {
+                    if (b.substr(2, 8) == "columns:") {
+                        columns = "";
+                        for (int l = 11; l < b.length(); l++) {
+                            columns += b[l];
+                            columns_n = stod(columns);
+                        }
+                    }
+                }
+                else if (b[0] == ' ') {
+                    k = 1;
+                    n1 = n2 = "";
+                    while (b[k] != ' ') {
+                        n1 += b[k];
+                        n1_n = stod(n1);
+                        k++;
+                    }
+                    k++;
+                    while (b[k] != ' ') {
+                        n2 += b[k];
+                        n2_n = stod(n2);
+                        k++;
+                    }
+                    step = n2_n - n1_n;
+                    name = "";
+                    plikzap << "distance\n" << n1 << ' ' << step << ' ' << columns_n - 1 << '\n';
+
+                }
+            }
+            else if (name == "deadZone") {
+                if (b[0] >= 48 && b[0] <= 57) {
+                    deadzone = b;
+                    plikzap << "deadZone\n" << deadzone << '\n';
+                }
+            }
+            else if (name == "signal") {
+                break;
+            }
         }
+        plik.close();
+
+        // signal
+        string binary_file_name = "results.bin";
+
+        ifstream signal_file(binary_file_name, ios::binary);
+        vector<double> results;
+        double value;
+
+        int k = 0;
+        while (signal_file.read(reinterpret_cast<char*>(&value), sizeof(double))) {
+            if (k % 36 == 5)
+                plikzap << value << "\n";
+            k++;
+        }
+
+        plikzap.close();
     }
-    // Ustawienie punktu startowego w pliku
-    plik.seekg(0, ios::end);
-    streampos file_end = plik.tellg();
-    streampos chunk_size = (file_end - signal_start) / LICZBA_WATKOW;
+    return 0;
+}
 
-    vector<vector<Result>> thread_results(LICZBA_WATKOW);
-
-    // Utwórz wątki
-    vector<thread> threads;
-    for (int i = 0; i < LICZBA_WATKOW; ++i) {
-        streampos chunk_start = signal_start + i * chunk_size;
-        streampos chunk_end = (i == LICZBA_WATKOW - 1) ? file_end : chunk_start + chunk_size;
-
-        threads.emplace_back([&, chunk_start, chunk_end, i]() {
-            fstream thread_file(file_name, ios::in);
-            process_file_chunk(thread_file, chunk_start, chunk_end, i + 1, ref(thread_results[i]));
-            });
+double string_to_double(string number) {
+    if (number.empty()) {
+        cerr << "Błąd: pusty ciąg w string_to_double!" << '\n';
+        return 0; // lub rzuć wyjątek
     }
-
-    // Czekanie na zakończenie wątków
-    for (auto& thread : threads) {
-        thread.join();
+    string nnumber;
+    long double new_number = 0;
+    int j = 0, l = 0;
+    int modifier = 1;
+    if (number[0] == '-') {
+        modifier = -1;
+        l = 1;
     }
-
-    // Scalenie wyników
-    vector<Result> final_results;
-    for (const auto& thread_results : thread_results) {
-        final_results.insert(final_results.end(), thread_results.begin(), thread_results.end());
+    for (int k = l; k < (7 < number.length() ? 5 : number.length()); k++) {
+        nnumber += number[k];
     }
-
-
-    // Zapis wyników do pliku
-    string binary_file_name = "results.bin";
-    ofstream output_file(binary_file_name, ios::binary | ios::trunc);
-
-    for (const auto& res : final_results) {
-        output_file.write(reinterpret_cast<const char*>(&res.modulus), sizeof(double));
-    }
-    output_file.close();
-
-
-    ifstream input_file(binary_file_name, ios::binary);
-    ofstream plik_dane("dane.txt");
-    vector<double> results;
-    double value;
-
-    int j = 0;
-    while (input_file.read(reinterpret_cast<char*>(&value), sizeof(double))) {
-        if (j % 36 == 5) 
-            plik_dane << value << "\n";
+    while (nnumber[j] != '.' && j < nnumber.length()) {
         j++;
     }
-    
-    return 0;
+    for (int k = j - 1; k >= 0; k--) {
+        new_number += ((int(nnumber[k]) - 48) * exp(j - k - 1));
+
+    }
+    for (int k = j + 1; k < nnumber.length(); k++) {
+        new_number += ((int(nnumber[k]) - 48) * exp(j - k));
+    }
+    new_number *= modifier;
+    return new_number;
 }
